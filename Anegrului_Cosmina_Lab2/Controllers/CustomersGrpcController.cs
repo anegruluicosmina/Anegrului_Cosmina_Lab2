a11a1d2638f55aabc620d52f5ba2f4ac17b7ceca
MARIA-COSMINA ANEGRULUI <maria.anegrului@stud.ubbcluj.ro>
2022-01-06 18:35:57 +0200

    lab12

---
+++ Anegrului_Cosmina_Lab2/Controllers/CustomersGrpcController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anegrului_Cosmina_Lab2.Controllers
+{
+    public class CustomersGrpcController : Controller
+    {
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Customer customer = client.Get(new CustomerId() { Id = (int)id });
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                var client = new CustomerService.CustomerServiceClient(channel);
+                Customer response = client.Update(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+    }
+}
